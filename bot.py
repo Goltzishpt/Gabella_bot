11149935217e0a0d3f283584e0993c2915ee206d
Goltzishpt <gilzagoltz@gmail.com>
2022-12-29 19:25:26 +0200

    bot

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import config
 
 api_id = config.API_ID
 api_hash = config.API_HASH
+token = config.BOT_TOKEN
 group = 'gabellastest'
 channel = 'gabellachannel'
 
-app = Client('my_account', api_id, api_hash)
+app = Client('my_account', api_id, api_hash, bot_token=token)
--- handlers.py
+++ handlers.py
@@ -1,25 +1,36 @@
 import asyncio
-from pyrogram import Client, filters
+from pyrogram import filters
+import pyrogram
 from bot import app
+from pyrogram import enums
 
 
-
-async def message_handler(client_obj, message_obj):
+async def message_handler(message_obj, type_message):
     print('get message from user, wait 5 sec')
-    await asyncio.sleep(5)
-    await client_obj.read_history(message_obj.chat.id)
     print('read the message from user')
-    await client_obj.send_chat_active(message_obj.chat.id, 'typing')
+    print('imitation typing...')
+    await asyncio.sleep(2)
+    print(type(message_obj))
+    await app.send_chat_action(message_obj.chat.id, type_message)
+    await asyncio.sleep(5)
+    if type_message == enums.ChatAction.TYPING:
+        await app.send_message(message_obj.chat.id, 'Привет \nЯ пока занята(или сплю) и не могу ответить. Напишу позже(c) Gabella-bot')
+    if type_message == enums.ChatAction.CHOOSE_STICKER:
+        await app.send_sticker(chat_id=message_obj.chat.id,
+                               sticker='CAACAgIAAxkBAAJrHmOtv3SnmTOTMRJPAAH46RvVjmdtQwACgxYAAlPr2EukI39bWKGsqR4E')
 
 
-@app.on_message(filters=filters.text)
+
+@app.on_message(filters=filters.text & filters.private)
 async def text_handler(client, message):
     print(f'Користувач відправив текст: {message.text}')
-    await message_handler(client, message)
+    await message_handler(message, enums.ChatAction.TYPING)
 
 
-@app.on_message(filters=filters.sticker)
+
+@app.on_message(filters=filters.sticker & filters.private)
 async def sticker_handler(client, message):
     print(f'Користувач відпрвив текст: {message.text}')
     print('Відповідаємо користувачу...')
-    await message.reply('sticker')
+    await message_handler(message, enums.ChatAction.CHOOSE_STICKER)
+
