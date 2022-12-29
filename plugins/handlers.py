68e3d8d4427e6272ce8ba36071d23cbdc62951ff
Goltzishpt <gilzagoltz@gmail.com>
2022-12-29 04:13:34 +0200

    git

--- plugins/handlers.py
+++ plugins/handlers.py
@@ -7,6 +7,7 @@ async def message_handler(client_obj, message_obj):
     await asyncio.sleep(5)
     await client_obj.read_history(message_obj.chat.id)
     print('read the message from user')
+    await client_obj.send_chat_active(message_obj.chat.id, 'typing')
 
 
 @Client.on_message(filters=filters.text)
